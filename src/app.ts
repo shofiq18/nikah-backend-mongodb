019800eec5b4a605505dcf802ef4710ed88ddbc8
shofiq18 <shafiqul.islam25021998@gmail.com>
2026-04-11 09:43:12 +0600

    CORS ADD

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,38 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import config from './config/index.js';
-import { uptime } from 'process';
-import { timeStamp } from 'console';
-import globalErrorHandler from './app/middlewares/globalErrorHandler.js';
-import notFound from './app/middlewares/notFound.js';
-import { UserRoutes } from './app/modules/user/user.route.js';
+import config from "./config/index.js";
+import { uptime } from "process";
+import { timeStamp } from "console";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler.js";
+import notFound from "./app/middlewares/notFound.js";
+import { UserRoutes } from "./app/modules/user/user.route.js";
 const app: Application = express();
-app.use(cors({
-    origin: 'https://nikahbd.vercel.app/',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://nikahbd.vercel.app", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  }),
+);
 
 //parser
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        message: "Server is running..",
-        environment: config.node_env,
-        uptime: process.uptime().toFixed(2) + " sec",
-        timeStamp: new Date().toISOString()
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Server is running..",
+    environment: config.node_env,
+    uptime: process.uptime().toFixed(2) + " sec",
+    timeStamp: new Date().toISOString(),
+  });
 });
 
 // ─── API ROUTES ──────────────────────────────────────────────────────
-app.use('/api/v1', UserRoutes);
-
-
+app.use("/api/v1", UserRoutes);
 
 app.use(globalErrorHandler);
 
